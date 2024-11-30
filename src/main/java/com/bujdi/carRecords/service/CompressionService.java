3c99a51c7302c6d1c350b923951fe11f9548ce98
Balazs Bujdoso <balazs.bujdoso@hrduo.com>
2024-11-30 13:07:59 +0000

    Image compression: use thumbnailer

--- src/main/java/com/bujdi/carRecords/service/CompressionService.java
+++ src/main/java/com/bujdi/carRecords/service/CompressionService.java
@@ -1,11 +1,11 @@
 package com.bujdi.carRecords.service;
 
 import com.bujdi.carRecords.utils.InMemoryMultipartFile;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -38,21 +38,9 @@ public class CompressionService {
         );
     }
 
-    private BufferedImage resizeImage(BufferedImage originalImage, int width, int height) {
-        // Ensure the new image has an appropriate color model
-        BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-        Graphics2D g = resizedImage.createGraphics();
-
-        // Apply high-quality rendering hints
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        // Perform the scaling
-        g.drawImage(originalImage, 0, 0, width, height, null);
-
-        g.dispose();
-        return resizedImage;
+    private BufferedImage resizeImage(BufferedImage originalImage, int width, int height) throws IOException {
+        return Thumbnails.of(originalImage)
+                .size(width, height)
+                .asBufferedImage();
     }
 }
